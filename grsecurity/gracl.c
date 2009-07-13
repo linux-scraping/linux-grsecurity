e6700d6e7727f9585c890df175bb2a4ec7b12b8c
Brad Spengler <spender@grsecurity.net>
2009-07-12 22:14:26 -0400

    grsec: Apply grsecurity-2.1.14-2.6.29.6-200907122214.patch
    Signature-tree: 7f1c635f611f2132b08b4f8c28081f874d0cbbf7

--- grsecurity/gracl.c
+++ grsecurity/gracl.c
@@ -3757,7 +3757,7 @@ int gr_is_taskstats_denied(int pid)
 		if (cred->uid != 0)
 			ret = -EACCES;
 #elif defined(CONFIG_GRKERNSEC_PROC_USERGROUP)
-		if (cred->uid != 0 && !groups_search(cred->group_info, CONFIG_GRKERNSEC_PROC_GID)
+		if (cred->uid != 0 && !groups_search(cred->group_info, CONFIG_GRKERNSEC_PROC_GID))
 			ret = -EACCES;
 #endif
 #endif
