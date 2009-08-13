b6b8459608bcf700048251b3ef0b554c6035c8c7
Brad Spengler <spender@grsecurity.net>
2009-08-12 21:19:47 -0400

    grsec: Apply grsecurity-2.1.14-2.6.30.4-200908122115.patch
    Signature-tree: b13c85faf94339f7037a12de482d43e3d8caf6e0

--- arch/x86/kernel/vmi_32.c
+++ arch/x86/kernel/vmi_32.c
@@ -466,7 +466,7 @@ vmi_startup_ipi_hook(int phys_apicid, unsigned long start_eip,
 	ap.ds = __KERNEL_DS;
 	ap.es = __KERNEL_DS;
 	ap.fs = __KERNEL_PERCPU;
-	ap.gs = 0;
+	ap.gs = __KERNEL_STACK_CANARY;
 
 	ap.eflags = 0;
 
