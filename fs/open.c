0fa584dc0b87aa1494dbe616ba45b98b8b387053
Brad Spengler <spender@grsecurity.net>
2010-11-06 21:04:54 -0400

    grsec: Apply grsecurity-2.2.0-2.6.32.25-201011062054.patch
    Signature-tree: 9af2f010378d870498b78a84231dc9156861e8e2

--- fs/open.c
+++ fs/open.c
@@ -643,27 +643,27 @@ SYSCALL_DEFINE2(fchmod, unsigned int, fd, mode_t, mode)
 	if (err)
 		goto out_putf;
 
+	mutex_lock(&inode->i_mutex);
+
 	if (!gr_acl_handle_fchmod(dentry, file->f_path.mnt, mode)) {
 		err = -EACCES;
-		goto out_drop_write;
+		goto out_unlock;
 	}
 
-	mutex_lock(&inode->i_mutex);
 	if (mode == (mode_t) -1)
 		mode = inode->i_mode;
 
 	if (gr_handle_chroot_chmod(dentry, file->f_path.mnt, mode)) {
 		err = -EPERM;
-		mutex_unlock(&inode->i_mutex);
-		goto out_drop_write;
+		goto out_unlock;
 	}
 
 	newattrs.ia_mode = (mode & S_IALLUGO) | (inode->i_mode & ~S_IALLUGO);
 	newattrs.ia_valid = ATTR_MODE | ATTR_CTIME;
 	err = notify_change(dentry, &newattrs);
-	mutex_unlock(&inode->i_mutex);
 
-out_drop_write:
+out_unlock:
+	mutex_unlock(&inode->i_mutex);
 	mnt_drop_write(file->f_path.mnt);
 out_putf:
 	fput(file);
@@ -687,27 +687,27 @@ SYSCALL_DEFINE3(fchmodat, int, dfd, const char __user *, filename, mode_t, mode)
 	if (error)
 		goto dput_and_out;
 
+	mutex_lock(&inode->i_mutex);
+
 	if (!gr_acl_handle_chmod(path.dentry, path.mnt, mode)) {
 		error = -EACCES;
-		goto out_drop_write;
+		goto out_unlock;
 	}
 
-	mutex_lock(&inode->i_mutex);
 	if (mode == (mode_t) -1)
 		mode = inode->i_mode;
 
 	if (gr_handle_chroot_chmod(path.dentry, path.mnt, mode)) {
 		error = -EACCES;
-		mutex_unlock(&inode->i_mutex);
-		goto out_drop_write;
+		goto out_unlock;
 	}
 
 	newattrs.ia_mode = (mode & S_IALLUGO) | (inode->i_mode & ~S_IALLUGO);
 	newattrs.ia_valid = ATTR_MODE | ATTR_CTIME;
 	error = notify_change(path.dentry, &newattrs);
-	mutex_unlock(&inode->i_mutex);
 
-out_drop_write:
+out_unlock:
+	mutex_unlock(&inode->i_mutex);
 	mnt_drop_write(path.mnt);
 dput_and_out:
 	path_put(&path);
