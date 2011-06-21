d2d6619bfeb3a6ecc6248ce87d8dfedc7b74b178
Brad Spengler <spender@grsecurity.net>
2011-06-21 18:05:46 -0400

    grsec: Apply grsecurity-2.2.2-2.6.39.1-201106211740.patch
    Changes in grsecurity-2.2.2-2.6.39.1-201106211740.patch:
    ---------------------------------------------------------------
    backport security fixes from upstream
    disable PROC_CONNECTOR
    
     drivers/connector/Kconfig |    4 ++--
     grsecurity/grsec_chroot.c |    2 +-
     grsecurity/grsec_mount.c  |    2 +-
     mm/ksm.c                  |    6 ++++++
     net/ipv4/inet_diag.c      |   14 ++++++--------
     security/tomoyo/mount.c   |    2 +-
     8 files changed, 19 insertions(+), 14 deletions(-)
    ---------------------------------------------------------------
    
    Signature-tree: 831b3b8a642765eef0b0e9c91c970e9ad7e4d5d6

--- grsecurity/grsec_mount.c
+++ grsecurity/grsec_mount.c
@@ -29,7 +29,7 @@ gr_log_mount(const char *from, const char *to, const int retval)
 {
 #ifdef CONFIG_GRKERNSEC_AUDIT_MOUNT
 	if (grsec_enable_mount && (retval >= 0))
-		gr_log_str_str(GR_DO_AUDIT, GR_MOUNT_AUDIT_MSG, from, to);
+		gr_log_str_str(GR_DO_AUDIT, GR_MOUNT_AUDIT_MSG, from ? from : "none", to);
 #endif
 	return;
 }
