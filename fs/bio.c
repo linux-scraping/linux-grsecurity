20d8e3296af54aabfac39b8019ece0645d8a3990
Brad Spengler <spender@grsecurity.net>
2012-03-24 20:37:32 -0400

    grsec: Apply grsecurity-2.9-2.6.32.59-201203242025.patch
    commit 22d074672125f6a246d65a77353206c59aa1385a
    Merge: 5a975e9 b3f064e
    Author: Brad Spengler <spender@grsecurity.net>
    Date:   Sat Mar 24 19:37:19 2012 -0400
    
        Merge branch 'pax-stable' into grsec-stable
    
    commit b3f064e6423ded0dcba59eecd1dfb05bd470c38d
    Author: Brad Spengler <spender@grsecurity.net>
    Date:   Sat Mar 24 19:36:57 2012 -0400
    
        Update to pax-linux-2.6.32.59-test154.patch
        reduces overcommit amount from recently increased brk entropy
    
     arch/arm/include/asm/atomic.h |    5 +++++
     fs/binfmt_elf.c               |   27 ++++++++++++++++++++++-----
     fs/bio.c                      |    2 +-
     3 files changed, 28 insertions(+), 6 deletions(-)
    
    Signature-tree: e0a2a144f5ea3f617e3d99393d89e1145e07befe

--- fs/bio.c
+++ fs/bio.c
@@ -841,7 +841,7 @@ struct bio *bio_copy_user_iov(struct request_queue *q,
 		/*
 		 * Overflow, abort
 		 */
-		if (end < start)
+		if (end < start || end - start > INT_MAX - nr_pages)
 			return ERR_PTR(-EINVAL);
 
 		nr_pages += end - start;
