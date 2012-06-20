176c1af4602bf833a170521026399d06fa022298
Brad Spengler <spender@grsecurity.net>
2012-06-19 22:27:30 -0400

    grsec: Apply grsecurity-2.9.1-2.6.32.59-201206192223.patch
    commit 2d96eaa3853bcee441719df8521997dec28c61a2
    Author: Brad Spengler <spender@grsecurity.net>
    Date:   Tue Jun 19 21:55:07 2012 -0400
    
        add grsecurity allocators to size overflow table
        change sorting of overflow table
    
     tools/gcc/generate_size_overflow_hash.sh |    2 +-
     tools/gcc/size_overflow_hash.data        |    4 ++++
     2 files changed, 5 insertions(+), 1 deletions(-)
    
    commit 8ecbb955fc0416278ad04e077d37a5333b806b44
    Merge: b01fa4c 39999b7
    Author: Brad Spengler <spender@grsecurity.net>
    Date:   Tue Jun 19 20:14:19 2012 -0400
    
        Merge branch 'pax-stable' into grsec-stable
    
    commit 39999b77e582ac59e70f93bff3b284f25313f8ac
    Author: Brad Spengler <spender@grsecurity.net>
    Date:   Tue Jun 19 20:12:51 2012 -0400
    
        Update to pax-linux-2.6.32.59-test176.patch
    
     Documentation/dontdiff                   |    1 +
     tools/gcc/Makefile                       |   10 +
     tools/gcc/generate_size_overflow_hash.sh |   94 +
     tools/gcc/size_overflow_hash.data        | 1847 +++++
     tools/gcc/size_overflow_hash.h           |13266 ------------------------------
     tools/gcc/size_overflow_plugin.c         |   15 +-
     6 files changed, 1959 insertions(+), 13274 deletions(-)
    
    Signature-tree: 56fc66f0650ae6dc3655abd43f3a850c915623af

---
+++ tools/gcc/generate_size_overflow_hash.sh
@@ -0,0 +1,94 @@
+#!/bin/bash
+
+# This script generates the hash table (size_overflow_hash.h) for the size_overflow gcc plugin (size_overflow_plugin.c).
+
+header1="size_overflow_hash.h"
+database="size_overflow_hash.data"
+n=65536
+
+usage() {
+cat <<EOF
+usage: $0 options
+OPTIONS:
+        -h|--help               help
+	-o			header file
+	-d			database file
+	-n			hash array size
+EOF
+    return 0
+}
+
+while true
+do
+    case "$1" in
+    -h|--help)	usage && exit 0;;
+    -n)		n=$2; shift 2;;
+    -o)		header1="$2"; shift 2;;
+    -d)		database="$2"; shift 2;;
+    --)		shift 1; break ;;
+     *)		break ;;
+    esac
+done
+
+create_defines() {
+	for i in `seq 1 10`
+	do
+		echo -e "#define PARAM"$i" (1U << "$i")" >> "$header1"
+	done
+	echo >> "$header1"
+}
+
+create_structs () {
+	rm -f "$header1"
+
+	create_defines
+
+	cat "$database" | while read data
+	do
+		data_array=($data)
+		struct_hash_name="${data_array[0]}"
+		funcn="${data_array[1]}"
+		params="${data_array[2]}"
+		next="${data_array[5]}"
+
+		echo "struct size_overflow_hash $struct_hash_name = {" >> "$header1"
+
+		echo -e "\t.next\t= $next,\n\t.name\t= \"$funcn\"," >> "$header1"
+		echo -en "\t.param\t= " >> "$header1"
+		line=
+		for param_num in ${params//-/ };
+		do
+			line="${line}PARAM"$param_num"|"
+		done
+
+		echo -e "${line%?},\n};\n" >> "$header1"
+	done
+}
+
+create_headers () {
+	echo "struct size_overflow_hash *size_overflow_hash[$n] = {" >> "$header1"
+}
+
+create_array_elements () {
+	index=0
+	grep -v "nohasharray" $database | sort -n -k 2,7 | while read data
+	do
+		data_array=($data)
+		i="${data_array[3]}"
+		hash="${data_array[4]}"
+		while [[ $index -lt $i ]]
+		do
+			echo -e "\t["$index"]\t= NULL," >> "$header1"
+			index=$(($index + 1))
+		done
+		index=$(($index + 1))
+		echo -e "\t["$i"]\t= &"$hash"," >> "$header1"
+	done
+	echo '};' >> $header1
+}
+
+create_structs
+create_headers
+create_array_elements
+
+exit 0
