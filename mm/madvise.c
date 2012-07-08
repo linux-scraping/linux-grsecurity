f4efe23e5d171b7c94bf81e46f93897022b021a0
Brad Spengler <spender@grsecurity.net>
2012-07-08 09:25:44 -0400

    grsec: Apply grsecurity-2.9.1-2.6.32.59-201207080923.patch
    commit efce57e2f0b61c225449bc624afdb9034a31303b
    Author: Brad Spengler <spender@grsecurity.net>
    Date:   Sat Jul 7 20:30:28 2012 -0400
    
        Backport security fix:
        http://git.kernel.org/?p=linux/kernel/git/torvalds/linux.git;a=commitdiff;h=9ab4233dd08036fe34a89c7dc6f47a8bf2eb29eb
    
        author	Andy Lutomirski <luto@amacapital.net>
        Thu, 5 Jul 2012 23:00:11 +0000 (16:00 -0700)
        committer	Linus Torvalds <torvalds@linux-foundation.org>
        Fri, 6 Jul 2012 17:34:38 +0000 (10:34 -0700)
        Otherwise the code races with munmap (causing a use-after-free
        of the vma) or with close (causing a use-after-free of the struct
        file).
    
        The bug was introduced by commit 90ed52ebe481 ("[PATCH] holepunch: fix
        mmap_sem i_mutex deadlock")
    
        Cc: Hugh Dickins <hugh@veritas.com>
        Cc: Miklos Szeredi <mszeredi@suse.cz>
        Cc: Badari Pulavarty <pbadari@us.ibm.com>
        Cc: Nick Piggin <npiggin@suse.de>
        Cc: stable@vger.kernel.org
        Signed-off-by: Andy Lutomirski <luto@amacapital.net>
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
     mm/madvise.c |   19 ++++++++++++++-----
     1 files changed, 14 insertions(+), 5 deletions(-)
    
    Signature-tree: b6e7aab3ab13a6c58a2f6a1acbd17840b90c1793

--- mm/madvise.c
+++ mm/madvise.c
@@ -12,6 +12,8 @@
 #include <linux/hugetlb.h>
 #include <linux/sched.h>
 #include <linux/ksm.h>
+#include <linux/fs.h>
+#include <linux/file.h>
 
 /*
  * Any behaviour which results in changes to the vma->vm_flags needs to
@@ -221,16 +223,17 @@ static long madvise_remove(struct vm_area_struct *vma,
 	struct address_space *mapping;
 	loff_t offset, endoff;
 	int error;
+	struct file *f;
 
 	*prev = NULL;	/* tell sys_madvise we drop mmap_sem */
 
 	if (vma->vm_flags & (VM_LOCKED|VM_NONLINEAR|VM_HUGETLB))
 		return -EINVAL;
 
-	if (!vma->vm_file || !vma->vm_file->f_mapping
-		|| !vma->vm_file->f_mapping->host) {
-			return -EINVAL;
-	}
+	f = vma->vm_file;
+
+	if (!f || !f->f_mapping || !f->f_mapping->host)
+		return -EINVAL;
 
 	if ((vma->vm_flags & (VM_SHARED|VM_WRITE)) != (VM_SHARED|VM_WRITE))
 		return -EACCES;
@@ -242,10 +245,16 @@ static long madvise_remove(struct vm_area_struct *vma,
 	endoff = (loff_t)(end - vma->vm_start - 1)
 			+ ((loff_t)vma->vm_pgoff << PAGE_SHIFT);
 
-	/* vmtruncate_range needs to take i_mutex and i_alloc_sem */
+	/* vmtruncate_range needs to take i_mutex and i_alloc_sem. We need to
+	* explicitly grab a reference because the vma (and hence the
+	* vma's reference to the file) can go away as soon as we drop
+	* mmap_sem.
+	*/
+	get_file(f);
 	up_read(&current->mm->mmap_sem);
 	error = vmtruncate_range(mapping->host, offset, endoff);
 	down_read(&current->mm->mmap_sem);
+	fput(f);
 	return error;
 }
 
