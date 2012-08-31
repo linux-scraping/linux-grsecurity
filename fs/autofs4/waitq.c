51444942d4d1317e540dc38c8a08542b20f81f2a
Brad Spengler <spender@grsecurity.net>
2012-08-30 20:15:45 -0400

    grsec: Apply grsecurity-2.9.1-3.2.28-201208302014.patch
    commit 7b8d6b3a52c089ca9d4d5343a284d2a6c5741e06
    Author: Brad Spengler <spender@grsecurity.net>
    Date:   Thu Aug 30 20:12:03 2012 -0400
    
        Fix an infoleak discovered by my recent HIDESYM improvement and reported by Niki Guldbrand
    
     fs/autofs4/waitq.c |    9 +++++++++
     1 files changed, 9 insertions(+), 0 deletions(-)
    
    Signature-tree: 9a8d679be03eea41bf7f85714849e0e55bfb95c6

--- fs/autofs4/waitq.c
+++ fs/autofs4/waitq.c
@@ -338,6 +338,10 @@ static int validate_request(struct autofs_wait_queue **wait,
 	return 1;
 }
 
+#ifdef CONFIG_GRKERNSEC_HIDESYM
+static atomic_unchecked_t autofs_dummy_name_id = ATOMIC_INIT(0);
+#endif
+
 int autofs4_wait(struct autofs_sb_info *sbi, struct dentry *dentry,
 		enum autofs_notify notify)
 {
@@ -371,7 +375,12 @@ int autofs4_wait(struct autofs_sb_info *sbi, struct dentry *dentry,
 
 	/* If this is a direct mount request create a dummy name */
 	if (IS_ROOT(dentry) && autofs_type_trigger(sbi->type))
+#ifdef CONFIG_GRKERNSEC_HIDESYM
+		/* this name does get written to userland via autofs4_write() */
+		qstr.len = sprintf(name, "%08lx", atomic_inc_return_unchecked(&autofs_dummy_name_id));
+#else
 		qstr.len = sprintf(name, "%p", dentry);
+#endif
 	else {
 		qstr.len = autofs4_getpath(sbi, dentry, &name);
 		if (!qstr.len) {
