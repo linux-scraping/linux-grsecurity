fd6efa3313d1fe2228353e3daf0c513b4a321753
Brad Spengler <spender@grsecurity.net>
2012-10-12 19:15:26 -0400

    grsec: Apply grsecurity-2.9.1-3.2.31-201210121914.patch
    commit 10a4c08976d0d12e1dcea2799228e7b0cd88f353
    Author: Brad Spengler <spender@grsecurity.net>
    Date:   Fri Oct 12 18:13:12 2012 -0400
    
        Additional fixes for harmless stack traces reported by HIDESYM as seq_files
        are handled generically
    
     kernel/cgroup.c           |    2 +-
     kernel/kprobes.c          |    4 ++--
     kernel/lockdep_proc.c     |   10 +++++-----
     kernel/time/timer_stats.c |    2 +-
     4 files changed, 9 insertions(+), 9 deletions(-)
    
    Signature-tree: e35e24bbdb4fe5151bf8b9afda19be85b2771150

--- kernel/time/timer_stats.c
+++ kernel/time/timer_stats.c
@@ -275,7 +275,7 @@ static void print_name_offset(struct seq_file *m, unsigned long addr)
 	char symname[KSYM_NAME_LEN];
 
 	if (lookup_symbol_name(addr, symname) < 0)
-		seq_printf(m, "<%p>", (void *)addr);
+		seq_printf(m, "<%pK>", (void *)addr);
 	else
 		seq_printf(m, "%s", symname);
 #endif
