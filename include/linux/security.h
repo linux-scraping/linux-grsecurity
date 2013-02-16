9534f6ed0de820c701b4650343dac17175d895ff
Brad Spengler <spender@grsecurity.net>
2013-02-16 14:37:28 -0500

    grsec: Apply grsecurity-2.9.1-3.2.38-201302161435.patch
    commit 12b830c785b1d32521538addec8a5d827a8482ef
    Author: Brad Spengler <spender@grsecurity.net>
    Date:   Sat Feb 16 13:54:20 2013 -0500
    
        add security_capable_noaudit
    
     include/linux/security.h |    8 ++++++++
     1 files changed, 8 insertions(+), 0 deletions(-)
    
    commit bfca3ea61b28f8b94b663ccdce4895cfc23d549f
    Author: Brad Spengler <spender@grsecurity.net>
    Date:   Sat Feb 16 13:32:42 2013 -0500
    
        Make capable_nolog call security_capable_noaudit so that SELinux doesn't
        complain about capabilities that aren't necessarily used.
        Patch from Luis Ressel
    
        Conflicts:
    
        	kernel/capability.c
    
     kernel/capability.c |    4 ++--
     1 files changed, 2 insertions(+), 2 deletions(-)
    
    Signature-tree: 931eca6af29fd821220125a9f977019b2670fca7

--- include/linux/security.h
+++ include/linux/security.h
@@ -1677,6 +1677,8 @@ int security_capset(struct cred *new, const struct cred *old,
 		    const kernel_cap_t *permitted);
 int security_capable(struct user_namespace *ns, const struct cred *cred,
 			int cap);
+int security_capable_noaudit(struct user_namespace *ns, const struct cred *cred,
+				int cap);
 int security_real_capable(struct task_struct *tsk, struct user_namespace *ns,
 			int cap);
 int security_real_capable_noaudit(struct task_struct *tsk,
@@ -1879,6 +1881,12 @@ static inline int security_capable(struct user_namespace *ns,
 				   const struct cred *cred, int cap)
 {
 	return cap_capable(current, cred, ns, cap, SECURITY_CAP_AUDIT);
+}
+
+static inline int security_capable_noaudit(struct user_namespace *ns,
+				   const struct cred *cred, int cap)
+{
+	return cap_capable(current, cred, ns, cap, SECURITY_CAP_NOAUDIT);
 }
 
 static inline int security_real_capable(struct task_struct *tsk, struct user_namespace *ns, int cap)
