6c666cc59be9a403132c5a1a458b3c53c0d564fd
Brad Spengler <spender@grsecurity.net>
2013-06-27 20:57:52 -0400

    grsec: Apply grsecurity-2.9.1-2.6.32.61-201306272055.patch
    commit 0c7950f7aa8d391342879c4a06dd97fb4b1d6bed
    Author: Brad Spengler <spender@grsecurity.net>
    Date:   Thu Jun 27 20:26:21 2013 -0400
    
        Backport upstream commit: 300b962e5244a1ea010df7e88595faa0085b461d
    
     net/bluetooth/l2cap.c |    3 +++
     1 files changed, 3 insertions(+), 0 deletions(-)
    
    commit d8b95b76a3d38d6887ead483889f67bafe717eaa
    Author: Eric Dumazet <edumazet@google.com>
    Date:   Wed Jun 26 04:15:07 2013 -0700
    
        Upstream commit: a963a37d384d71ad43b3e9e79d68d42fbe0901f3
    
        ipv6: ip6_sk_dst_check() must not assume ipv6 dst
    
        It's possible to use AF_INET6 sockets and to connect to an IPv4
        destination. After this, socket dst cache is a pointer to a rtable,
        not rt6_info.
    
        ip6_sk_dst_check() should check the socket dst cache is IPv6, or else
        various corruptions/crashes can happen.
    
        Dave Jones can reproduce immediate crash with
        trinity -q -l off -n -c sendmsg -c connect
    
        With help from Hannes Frederic Sowa
    
        Reported-by: Dave Jones <davej@redhat.com>
        Reported-by: Hannes Frederic Sowa <hannes@stressinduktion.org>
        Signed-off-by: Eric Dumazet <edumazet@google.com>
        Acked-by: Hannes Frederic Sowa <hannes@stressinduktion.org>
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
     net/ipv6/ip6_output.c |    8 +++++++-
     1 files changed, 7 insertions(+), 1 deletions(-)
    
    commit 5c551560b64a2c67ef6b5724139196d174145f1c
    Author: Zefan Li <lizefan@huawei.com>
    Date:   Wed Jun 26 15:29:54 2013 +0800
    
        Upstream commit: 11eb2645cbf38a08ae491bf6c602eea900ec0bb5
    
        dlci: acquire rtnl_lock before calling __dev_get_by_name()
    
        Otherwise the net device returned can be freed at anytime.
    
        Signed-off-by: Li Zefan <lizefan@huawei.com>
        Cc: stable@vger.kernel.org
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
        Conflicts:
    
        	drivers/net/wan/dlci.c
    
     drivers/net/wan/dlci.c |   22 +++++++---------------
     1 files changed, 7 insertions(+), 15 deletions(-)
    
    commit f85ebadebb5bf2f36d65db1e7f31c86175339165
    Author: Zefan Li <lizefan@huawei.com>
    Date:   Wed Jun 26 15:31:58 2013 +0800
    
        Upstream commit: 578a1310f2592ba90c5674bca21c1dbd1adf3f0a
    
        dlci: validate the net device in dlci_del()
    
        We triggered an oops while running trinity with 3.4 kernel:
    
        BUG: unable to handle kernel paging request at 0000000100000d07
        IP: [<ffffffffa0109738>] dlci_ioctl+0xd8/0x2d4 [dlci]
        PGD 640c0d067 PUD 0
        Oops: 0000 [#1] PREEMPT SMP
        CPU 3
        ...
        Pid: 7302, comm: trinity-child3 Not tainted 3.4.24.09+ 40 Huawei Technologies Co., Ltd. Tecal RH2285          /BC11BTSA
        RIP: 0010:[<ffffffffa0109738>]  [<ffffffffa0109738>] dlci_ioctl+0xd8/0x2d4 [dlci]
        ...
        Call Trace:
          [<ffffffff8137c5c3>] sock_ioctl+0x153/0x280
          [<ffffffff81195494>] do_vfs_ioctl+0xa4/0x5e0
          [<ffffffff8118354a>] ? fget_light+0x3ea/0x490
          [<ffffffff81195a1f>] sys_ioctl+0x4f/0x80
          [<ffffffff81478b69>] system_call_fastpath+0x16/0x1b
        ...
    
        It's because the net device is not a dlci device.
    
        Reported-by: Li Jinyue <lijinyue@huawei.com>
        Signed-off-by: Li Zefan <lizefan@huawei.com>
        Cc: stable@vger.kernel.org
        Signed-off-by: David S. Miller <davem@davemloft.net>
    
     drivers/net/wan/dlci.c |   12 ++++++++++++
     1 files changed, 12 insertions(+), 0 deletions(-)
    
    Signature-tree: 4b1ec7f017d949d5614f651583dca6b9aaa853a0

--- drivers/net/wan/dlci.c
+++ drivers/net/wan/dlci.c
@@ -382,20 +382,24 @@ static int dlci_del(struct dlci_add *dlci)
 	struct net_device	*master, *slave;
 	int			err;
 
+	rtnl_lock();
+
 	/* validate slave device */
 	master = __dev_get_by_name(&init_net, dlci->devname);
-	if (!master)
-		return(-ENODEV);
+	if (!master) {
+		err = -ENODEV;
+		goto out;
+	}
 
 	if (netif_running(master)) {
-		return(-EBUSY);
+		err = -EBUSY;
+		goto out;
 	}
 
 	dlp = netdev_priv(master);
 	slave = dlp->slave;
 	flp = netdev_priv(slave);
 
-	rtnl_lock();
 	err = (*flp->deassoc)(slave, master);
 	if (!err) {
 		list_del(&dlp->list);
@@ -404,9 +408,9 @@ static int dlci_del(struct dlci_add *dlci)
 
 		dev_put(slave);
 	}
+out:
 	rtnl_unlock();
-
-	return(err);
+	return err;
 }
 
 static int dlci_ioctl(unsigned int cmd, void __user *arg)
