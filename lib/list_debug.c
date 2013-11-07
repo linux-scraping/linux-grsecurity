cd24a8a55d5b11eaf6e64c0c0d5a9daf4158a976
Brad Spengler <spender@grsecurity.net>
2013-11-07 16:49:13 -0500

    grsec: Apply grsecurity-2.9.1-3.2.52-201311071633.patch
    commit be3f3539e5745da7f0a4322e0cef77d763a91793
    Author: Brad Spengler <spender@grsecurity.net>
    Date:   Wed Nov 6 16:23:36 2013 -0500
    
        Force on DEBUG_LIST so all users can benefit from safe linking/unlinking
    
     security/Kconfig |    1 +
     1 files changed, 1 insertions(+), 0 deletions(-)
    
    commit 1457cd7c50771a37f297e5d5963a003f08134793
    Author: Brad Spengler <spender@grsecurity.net>
    Date:   Wed Nov 6 16:19:21 2013 -0500
    
        change DEBUG_LIST WARNs back to BUGs so they can benefit from the kernel
        bruteforce deterrence
    
        Conflicts:
    
        	lib/list_debug.c
    
     lib/list_debug.c |   65 ++++++++++++++++++++++++++++++++++-------------------
     1 files changed, 42 insertions(+), 23 deletions(-)
    
    commit 3b039667e9f82697a42754d5e78a3772757097d5
    Author: Dan Carpenter <dan.carpenter@oracle.com>
    Date:   Tue Oct 29 22:06:04 2013 +0300
    
        Upstream commit: 201f99f170df14ba52ea4c52847779042b7a623b
    
        uml: check length in exitcode_proc_write()
    
        We don't cap the size of buffer from the user so we could write past the
        end of the array here.  Only root can write to this file.
    
        Reported-by: Nico Golde <nico@ngolde.de>
        Reported-by: Fabian Yamaguchi <fabs@goesec.de>
        Signed-off-by: Dan Carpenter <dan.carpenter@oracle.com>
        Cc: stable@kernel.org
        Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    
     arch/um/kernel/exitcode.c |    4 +++-
     1 files changed, 3 insertions(+), 1 deletions(-)
    
    Signature-tree: 3eb8692c3b9448b34f27132d8da8b060c3ed7572

--- lib/list_debug.c
+++ lib/list_debug.c
@@ -22,18 +22,26 @@ static bool __list_add_debug(struct list_head *new,
 			     struct list_head *prev,
 			     struct list_head *next)
 {
-	if (WARN(next->prev != prev,
-		"list_add corruption. next->prev should be "
-		"prev (%p), but was %p. (next=%p).\n",
-		prev, next->prev, next) ||
-	    WARN(prev->next != next,
-		"list_add corruption. prev->next should be "
-		"next (%p), but was %p. (prev=%p).\n",
-		next, prev->next, prev) ||
-	    WARN(new == prev || new == next,
-		"list_add double add: new=%p, prev=%p, next=%p.\n",
-		new, prev, next))
+	if (unlikely(next->prev != prev)) {
+		printk(KERN_ERR "list_add corruption. next->prev should be "
+			"prev (%p), but was %p. (next=%p).\n",
+			prev, next->prev, next);
+		BUG();
 		return false;
+	}
+	if (unlikely(prev->next != next)) {
+		printk(KERN_ERR "list_add corruption. prev->next should be "
+			"next (%p), but was %p. (prev=%p).\n",
+			next, prev->next, prev);
+		BUG();
+		return false;
+	}
+	if (unlikely(new == prev || new == next)) {
+		printk(KERN_ERR "list_add double add: new=%p, prev=%p, next=%p.\n",
+			new, prev, next);
+		BUG();
+		return false;
+	}
 	return true;
 }
 
@@ -58,19 +66,30 @@ static bool __list_del_entry_debug(struct list_head *entry)
 	prev = entry->prev;
 	next = entry->next;
 
-	if (WARN(next == LIST_POISON1,
-		"list_del corruption, %p->next is LIST_POISON1 (%p)\n",
-		entry, LIST_POISON1) ||
-	    WARN(prev == LIST_POISON2,
-		"list_del corruption, %p->prev is LIST_POISON2 (%p)\n",
-		entry, LIST_POISON2) ||
-	    WARN(prev->next != entry,
-		"list_del corruption. prev->next should be %p, "
-		"but was %p\n", entry, prev->next) ||
-	    WARN(next->prev != entry,
-		"list_del corruption. next->prev should be %p, "
-		"but was %p\n", entry, next->prev))
+	if (unlikely(next == LIST_POISON1)) {
+		printk(KERN_ERR "list_del corruption, %p->next is LIST_POISON1 (%p)\n",
+			entry, LIST_POISON1);
+		BUG();
 		return false;
+	}
+	if (unlikely(prev == LIST_POISON2)) {
+		printk(KERN_ERR "list_del corruption, %p->prev is LIST_POISON2 (%p)\n",
+			entry, LIST_POISON2);
+		BUG();
+		return false;
+	}
+	if (unlikely(entry->prev->next != entry)) {
+		printk(KERN_ERR "list_del corruption. prev->next should be %p, "
+			"but was %p\n", entry, prev->next);
+		BUG();
+		return false;
+	}
+	if (unlikely(entry->next->prev != entry)) {
+		printk(KERN_ERR "list_del corruption. next->prev should be %p, "
+			"but was %p\n", entry, next->prev);
+		BUG();
+		return false;
+	}
 	return true;
 }
 
