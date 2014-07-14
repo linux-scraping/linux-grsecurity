2016c30a78c60033242303207d76c9f43373d27a
Brad Spengler <spender@grsecurity.net>
2014-07-13 20:29:30 -0400

    grsec: Apply grsecurity-3.0-3.2.61-201407132023.patch
    commit 862b3b49a8fed0720d62674f5ead9bd5eec69fa0
    Merge: 0b43139 9f35dc2
    Author: Brad Spengler <spender@grsecurity.net>
    Date:   Sun Jul 13 19:37:28 2014 -0400
    
        Merge branch 'pax-stable' into grsec-stable
    
    commit 9f35dc281dd37d104be973ad3bd47066b4662865
    Author: Brad Spengler <spender@grsecurity.net>
    Date:   Sun Jul 13 19:35:52 2014 -0400
    
        Update to pax-linux-3.2.61-test154.patch:
        - Emese fixed a size overflow false positive, reported by joe (http://forums.grsecurity.net/viewtopic.php?f=3&t=3997)
        - reverted an old compiler warning fix now causing problems with PCI device enumeration, reported by /dev/random (http://forums.grsecurity.net/viewtopic.php?f=3&t=3989)
    
     include/linux/mod_devicetable.h                    |    2 +-
     .../insert_size_overflow_check_ipa.c               |   18 +++++++++++++-----
     .../size_overflow_plugin/size_overflow_plugin.c    |    2 +-
     3 files changed, 15 insertions(+), 7 deletions(-)
    
    Signature-tree: 0d66d07c264a3ca14eacedbc4d8cc2eb3407e651

--- tools/gcc/size_overflow_plugin/size_overflow_plugin.c
+++ tools/gcc/size_overflow_plugin/size_overflow_plugin.c
@@ -30,7 +30,7 @@ tree size_overflow_type_DI;
 tree size_overflow_type_TI;
 
 static struct plugin_info size_overflow_plugin_info = {
-	.version	= "20140517",
+	.version	= "20140713",
 	.help		= "no-size-overflow\tturn off size overflow checking\n",
 };
 
