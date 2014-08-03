d8e9586e51b79d300eb47f66ea5419e8c4b4cdce
Brad Spengler <spender@grsecurity.net>
2014-08-03 11:31:38 -0400

    grsec: Apply grsecurity-3.0-3.14.15-201408031127.patch
    commit 0320af76fdaff58fc90bb385fc34fa7b4fddd56f
    Author: Brad Spengler <spender@grsecurity.net>
    Date:   Sun Aug 3 10:20:05 2014 -0400
    
        compile fix
    
     grsecurity/grsum.c |    2 +-
     1 files changed, 1 insertions(+), 1 deletions(-)
    
    commit 801ea0ffafe27a6dcf9ee74d829383144b198c50
    Author: Brad Spengler <spender@grsecurity.net>
    Date:   Sun Aug 3 10:15:31 2014 -0400
    
        fix retval
    
     grsecurity/grsum.c |    8 ++++++--
     1 files changed, 6 insertions(+), 2 deletions(-)
    
    commit 5c126813a33a262193a5ce20ff6ad000c279958c
    Author: Brad Spengler <spender@grsecurity.net>
    Date:   Sun Aug 3 10:00:44 2014 -0400
    
        Mark the right declarations __read_only
    
     grsecurity/gracl_policy.c |    6 +++---
     grsecurity/grsec_init.c   |    6 +++---
     2 files changed, 6 insertions(+), 6 deletions(-)
    
    commit 20bf7c8a7b7a65864e26ca09de3e7930a9681636
    Author: Brad Spengler <spender@grsecurity.net>
    Date:   Sun Aug 3 09:52:39 2014 -0400
    
        While on x86/x64 scatterlists can be created from kernel image buffers,
        on sparc64 this is apparently invalid and causes a system hang.
        Convert gr_usermode, gr_system_sum, and gr_system_salt back to using
        memory allocated at init.
    
        Many thanks to Blake Self for late night debugging help
    
        Conflicts:
    
        	grsecurity/gracl_policy.c
    
     grsecurity/gracl_policy.c |   60 ++++++++++++++++++++++----------------------
     grsecurity/grsec_init.c   |   14 ++++++++++
     grsecurity/grsum.c        |   37 +++++++++++++--------------
     3 files changed, 62 insertions(+), 49 deletions(-)
    
    Signature-tree: 16e5328b8160d753d763773072b480ff965176da

--- grsecurity/grsum.c
+++ grsecurity/grsum.c
@@ -14,47 +14,50 @@
 int
 chkpw(struct gr_arg *entry, unsigned char *salt, unsigned char *sum)
 {
-	char *p;
 	struct crypto_hash *tfm;
 	struct hash_desc desc;
-	struct scatterlist sg;
-	unsigned char temp_sum[GR_SHA_LEN];
-	volatile int retval = 0;
+	struct scatterlist sg[2];
+	unsigned char temp_sum[GR_SHA_LEN] __attribute__((aligned(__alignof__(unsigned long))));
+	unsigned long *tmpsumptr = (unsigned long *)temp_sum;
+	unsigned long *sumptr = (unsigned long *)sum;
+	int cryptres;
+	int retval = 1;
+	volatile int mismatched = 0;
 	volatile int dummy = 0;
 	unsigned int i;
-
-	sg_init_table(&sg, 1);
 
 	tfm = crypto_alloc_hash("sha256", 0, CRYPTO_ALG_ASYNC);
 	if (IS_ERR(tfm)) {
 		/* should never happen, since sha256 should be built in */
+		memset(entry->pw, 0, GR_PW_LEN);
 		return 1;
 	}
+
+	sg_init_table(sg, 2);
+	sg_set_buf(&sg[0], salt, GR_SALT_LEN);
+	sg_set_buf(&sg[1], entry->pw, strlen(entry->pw));
 
 	desc.tfm = tfm;
 	desc.flags = 0;
 
-	crypto_hash_init(&desc);
-
-	p = salt;
-	sg_set_buf(&sg, p, GR_SALT_LEN);
-	crypto_hash_update(&desc, &sg, sg.length);
-
-	p = entry->pw;
-	sg_set_buf(&sg, p, strlen(p));
-	
-	crypto_hash_update(&desc, &sg, sg.length);
-
-	crypto_hash_final(&desc, temp_sum);
+	cryptres = crypto_hash_digest(&desc, sg, GR_SALT_LEN + strlen(entry->pw),
+					temp_sum);
 
 	memset(entry->pw, 0, GR_PW_LEN);
 
-	for (i = 0; i < GR_SHA_LEN; i++)
-		if (sum[i] != temp_sum[i])
-			retval = 1;
+	if (cryptres)
+		goto out;
+
+	for (i = 0; i < GR_SHA_LEN/sizeof(tmpsumptr[0]); i++)
+		if (sumptr[i] != tmpsumptr[i])
+			mismatched = 1;
 		else
 			dummy = 1;	// waste a cycle
 
+	if (!mismatched)
+		retval = dummy - 1;
+
+out:
 	crypto_free_hash(tfm);
 
 	return retval;
