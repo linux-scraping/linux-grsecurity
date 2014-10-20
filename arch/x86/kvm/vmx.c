a165aee6ba6854fa843c6bdbb1ba8969344e9d91
Brad Spengler <spender@grsecurity.net>
2014-10-20 17:41:32 -0400

    grsec: Apply grsecurity-3.0-3.2.63-201410201736.patch
    commit 822662d8112a3078613bef60e41c2e0266526c65
    Author: Brad Spengler <spender@grsecurity.net>
    Date:   Mon Oct 20 17:34:00 2014 -0400
    
        Revert "x86,kvm,vmx: Preserve CR4 across VM entry"
    
        This reverts commit ae21ca789a180a1146d84440d0293f9f8dce5875.
    
     arch/x86/kvm/vmx.c |   16 ++--------------
     1 files changed, 2 insertions(+), 14 deletions(-)
    
    Signature-tree: 05ff60524259bbb66d5a87e1879b7cc57e51b0bf

--- arch/x86/kvm/vmx.c
+++ arch/x86/kvm/vmx.c
@@ -390,7 +390,6 @@ struct vcpu_vmx {
 		u16           fs_sel, gs_sel, ldt_sel;
 		int           gs_ldt_reload_needed;
 		int           fs_reload_needed;
-		unsigned long vmcs_host_cr4;	/* May not match real cr4 */
 	} host_state;
 	struct {
 		int vm86_active;
@@ -3646,17 +3645,13 @@ static void vmx_set_constant_host_state(void)
 	u32 low32, high32;
 	unsigned long tmpl;
 	struct desc_ptr dt;
-	unsigned long cr4;
 
 	vmcs_writel(HOST_CR0, read_cr0() | X86_CR0_TS);  /* 22.2.3 */
+	vmcs_writel(HOST_CR4, read_cr4());  /* 22.2.3, 22.2.5 */
+
 #ifndef CONFIG_PAX_PER_CPU_PGD
 	vmcs_writel(HOST_CR3, read_cr3());  /* 22.2.3  FIXME: shadow tables */
 #endif
-
-	/* Save the most likely value for this task's CR4 in the VMCS. */
-	cr4 = read_cr4();
-	vmcs_writel(HOST_CR4, cr4);			/* 22.2.3, 22.2.5 */
-	vmx->host_state.vmcs_host_cr4 = cr4;
 
 	vmcs_write16(HOST_CS_SELECTOR, __KERNEL_CS);  /* 22.2.4 */
 	vmcs_write16(HOST_DS_SELECTOR, __KERNEL_DS);  /* 22.2.4 */
@@ -6112,7 +6107,6 @@ static void atomic_switch_perf_msrs(struct vcpu_vmx *vmx)
 static void __noclone vmx_vcpu_run(struct kvm_vcpu *vcpu)
 {
 	struct vcpu_vmx *vmx = to_vmx(vcpu);
-	unsigned long cr4;
 
 	if (is_guest_mode(vcpu) && !vmx->nested.nested_run_pending) {
 		struct vmcs12 *vmcs12 = get_vmcs12(vcpu);
@@ -6142,12 +6136,6 @@ static void __noclone vmx_vcpu_run(struct kvm_vcpu *vcpu)
 		vmcs_writel(GUEST_RSP, vcpu->arch.regs[VCPU_REGS_RSP]);
 	if (test_bit(VCPU_REGS_RIP, (unsigned long *)&vcpu->arch.regs_dirty))
 		vmcs_writel(GUEST_RIP, vcpu->arch.regs[VCPU_REGS_RIP]);
-
-	cr4 = read_cr4();
-	if (unlikely(cr4 != vmx->host_state.vmcs_host_cr4)) {
-		vmcs_writel(HOST_CR4, cr4);
-		vmx->host_state.vmcs_host_cr4 = cr4;
-	}
 
 	/* When single-stepping over STI and MOV SS, we must clear the
 	 * corresponding interruptibility bits in the guest state. Otherwise
