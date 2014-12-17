c9bcdc18e2bbf3386d376241b494337ebf83dfdb
Brad Spengler <spender@grsecurity.net>
2014-12-17 07:02:39 -0500

    grsec: Apply grsecurity-3.0-3.2.65-201412170654.patch
    commit f0141de20a94881331289127085a3c37ee055eb1
    Author: Brad Spengler <spender@grsecurity.net>
    Date:   Tue Dec 16 17:18:16 2014 -0500
    
        Force support on for user extended attributes for filesystems that currently
        don't default to on to match up with other filesystems like ext4/xfs that do.
    
        We'll need this to make an upcoming addition to the paid kernel packages work
        without any additional configuration by the user.
    
        If there's a particular mountpoint you don't want to have user extended attributes
        enabled on for whatever reason, the "nouser_xattr" mount option will honor it.
    
     fs/ext2/super.c     |    8 +++-----
     fs/ext3/super.c     |    8 +++-----
     fs/reiserfs/super.c |    4 ++++
     3 files changed, 10 insertions(+), 10 deletions(-)
    
    Signature-tree: cdf478b1de188e7c02d9a59dfa744a464f9030f1

--- fs/reiserfs/super.c
+++ fs/reiserfs/super.c
@@ -1664,6 +1664,10 @@ static int reiserfs_fill_super(struct super_block *s, void *data, int silent)
 	REISERFS_SB(s)->s_mount_opt |= (1 << REISERFS_SMALLTAIL);
 	REISERFS_SB(s)->s_mount_opt |= (1 << REISERFS_ERROR_RO);
 	REISERFS_SB(s)->s_mount_opt |= (1 << REISERFS_BARRIER_FLUSH);
+#ifdef CONFIG_REISERFS_FS_XATTR
+	/* turn on user xattrs by default */
+	REISERFS_SB(s)->s_mount_opt |= (1 << REISERFS_XATTRS_USER);
+#endif
 	/* no preallocation minimum, be smart in
 	   reiserfs_file_write instead */
 	REISERFS_SB(s)->s_alloc_options.preallocmin = 0;
